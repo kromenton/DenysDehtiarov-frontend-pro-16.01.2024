c3c961e54f17ca2af2d12f1f47baacad3423a630
kromenton <dendeg@gmail.com>
2024-02-09 00:48:07 +0200

    homeWork3 =done

---
+++ homeWorks/hw3/3.js
@@ -0,0 +1,16 @@
+const num1 = +prompt('enter first number');
+const num2 = +prompt('enter second number');
+const result1 = num1 + num2;
+const result2 = num1 - num2;
+const result3 = num1 * num2;
+const result4 = num1 / num2;
+alert(
+  `${num1} + ${num2} = ${result1}   ${num1} - ${num2} = ${result2}   ${num1} + ${num2} = ${result3}   ${num1} + ${num2} = ${result4}`
+);
+alert(
+  `${num1} + ${num2} = ${result1} `.concat(
+    `${num1} - ${num2} = ${result2} `,
+    `${num1} * ${num2} = ${result3} `,
+    `${num1} / ${num2} = ${result4}`
+  )
+);

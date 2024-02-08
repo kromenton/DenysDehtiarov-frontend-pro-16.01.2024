53653c47e3308483362403df190f6a1c447d90d2
kromenton <dendeg@gmail.com>
2024-02-09 00:53:11 +0200

    homeWork3 = done

--- homeWorks/hw3/3.js
+++ homeWorks/hw3/3.js
@@ -5,7 +5,7 @@ const result2 = num1 - num2;
 const result3 = num1 * num2;
 const result4 = num1 / num2;
 alert(
-  `${num1} + ${num2} = ${result1}   ${num1} - ${num2} = ${result2}   ${num1} + ${num2} = ${result3}   ${num1} + ${num2} = ${result4}`
+  `${num1} + ${num2} = ${result1}   ${num1} - ${num2} = ${result2}   ${num1} * ${num2} = ${result3}   ${num1} / ${num2} = ${result4}`
 );
 alert(
   `${num1} + ${num2} = ${result1} `.concat(

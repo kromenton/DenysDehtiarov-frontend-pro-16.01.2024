700df122063a426dff7f91602d69425284b7c4ef
kromenton <dendeg@gmail.com>
2024-02-08 21:52:19 +0200

    done

---
+++ homeWorks/hw1/1.js
@@ -0,0 +1,7 @@
+// Запитати ім'я користувача за допомогою prompt
+const userName = prompt('Please enter your name:');
+
+// Вивести повідомлення з ім'ям користувача за допомогою alert
+alert(`Hello, ${userName ? userName : 'guest'}! How are you?`);
+
+console.log(userName);
